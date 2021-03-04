14ddc285f98e25295eb4294ab61614cf226d1cd0
Kamran <kamran.mustafa1991@gmail.com>
2021-03-04 11:28:22 +0500

    data types

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-var helloworld = 'Say wHi';
-console.log(helloworld);
+var myFirstString = 'hello';
+var myFirstNumber = 123;
+var myFirstboolean = true;
+var myFirstStringArray = ['first', 'second'];
+var myFirstNumberArray = [1, 2];
+var myFirstTupple = ['firstString', 2];
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["blue"] = 100] = "blue";
+})(Color || (Color = {}));
+;
+var myColor = Color.Green;
+var myFirstAny = 'hello';
+myFirstAny = 123;
+var myFirstUnion = true;
+myFirstUnion = 123;
+var myFirstNull = null;
+function error(message) {
+    throw new Error(message);
+}
+console.log(error('my first error message'));
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,2 +1,26 @@
-let helloworld =  'Say wHi';
-console.log(helloworld);
+// Data Types
+
+let myFirstString: string = 'hello';
+let myFirstNumber: number = 123;
+let myFirstboolean: boolean = true;
+let myFirstStringArray: string[] = ['first', 'second'];
+let myFirstNumberArray: number[] = [1, 2];
+let myFirstTupple = ['firstString', 2];
+enum Color {
+    Gray,
+    Green,
+    blue = 100
+};
+let myColor: Color = Color.Green;
+let myFirstAny  : any = 'hello';
+myFirstAny = 123;
+let myFirstUnion : number | boolean = true;
+myFirstUnion = 123;
+let myFirstNull : null = null;
+
+function error(message : string) : never {
+    throw new Error(message);
+}
+
+console.log(error('my first error message'));
+
